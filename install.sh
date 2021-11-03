1bdcbd6a7249c1bbde8360da053a4f2b1c3247b9
benrau87 <benrau87@users.noreply.github.com>
2021-11-03 16:07:25 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,8 @@ sudo apt-get update
 sudo apt-get install -y python3-dev python3-pip linux-headers-generic rpm python-dev unzip python-pip virtualbox virtualbox-dkms python-virtualenv git ansible
 sudo gem install winrm-elevated
 sudo gem install winrm
-sudo rpm -ivh vagrant_2.2.7_x86_64.rpm
+wget https://releases.hashicorp.com/vagrant/2.2.18/vagrant_2.2.18_x86_64.rpm
+sudo rpm -ivh vagrant_2.2.18_x86_64.rpm
 git clone https://github.com/splunk/attack_range_local && cd attack_range_local
 sudo ln -sfn /usr/bin/python3.6 /usr/bin/python
 sudo python -m pip install -r requirements.txt
