88896002ba171ee62c43d2a3c72252a40c53bd2a
benrau87 <benrau87@users.noreply.github.com>
2021-11-03 11:43:17 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,8 +4,7 @@ sudo apt-get install -y python3-dev python3-pip linux-headers-generic python-dev
 sudo gem install winrm-elevated
 sudo gem install winrm
 git clone https://github.com/splunk/attack_range_local && cd attack_range_local
-virtualenv -p python3 venv
-source venv/bin/activate
-python3 -m pip install -r requirements.txt
+ln -sfn /usr/bin/python3.6 /usr/bin/python
+python -m pip install -r requirements.txt
 ansible-galaxy collection install community.windows
 chmod +x attack_range_local.py
