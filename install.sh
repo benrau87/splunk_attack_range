c4381a86050bde96536cd372db4603e63c4e8d09
benrau87 <benrau87@users.noreply.github.com>
2021-11-03 16:13:29 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt-get update 
-sudo apt-get install -y python3-dev python3-pip linux-headers-generic rpm python-dev unzip python-pip virtualbox virtualbox-dkms python-virtualenv git ansible
+sudo apt-get install -y python3-dev python3-pip linux-headers-generic python-dev unzip python-pip virtualbox virtualbox-dkms python-virtualenv git ansible rpm
 sudo gem install winrm-elevated
 sudo gem install winrm
 sudo ln -sfn /usr/bin/python3.6 /usr/bin/python
