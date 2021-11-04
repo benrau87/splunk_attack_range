0c19e23d8352f80836cfb5b8b756fa4f2390b29d
benrau87 <benrau87@users.noreply.github.com>
2021-11-04 11:29:35 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,11 +2,13 @@
 echo -e "What would you like to use as the global password?"
 read PASS
 sed -i "s/replaceme/${PASS}/g" attack_range_local.conf
-echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
-sudo sysctl -p 
-sudo iptables -t nat -A PREROUTING -p tcp --dport 53 -j DNAT --to-destination 1.1.1.1:53
-sudo iptables -t nat -A PREROUTING -p udp --dport 53 -j DNAT --to-destination 1.1.1.1:53
-sudo iptables -t nat -A POSTROUTING -j MASQUERADE
+#echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
+#sudo sysctl -p 
+#sudo iptables -t nat -A PREROUTING -p tcp --dport 53 -j DNAT --to-destination 1.1.1.1:53
+#sudo iptables -t nat -A PREROUTING -p udp --dport 53 -j DNAT --to-destination 1.1.1.1:53
+#sudo iptables -t nat -A POSTROUTING -j MASQUERADE
+sudo iptables -t nat -A OUTPUT -p udp --dport 53 -j DNAT --to 1.1.1.1:53
+sudo iptables -t nat -A OUTPUT -p tcp --dport 53 -j DNAT --to 1.1.1.1:53
 sudo apt-get update 
 sudo apt-get install -y python3-dev python3-pip linux-headers-generic python-dev unzip python-pip virtualbox virtualbox-dkms python-virtualenv git ansible rpm
 sudo gem install winrm-elevated
